2df5708cdedfd53036000616e265be91aff79c52
Rhaíssa Zeferino <66739812+RhaissaZeferino@users.noreply.github.com>
2022-03-25 08:56:27 -0300

    Update randomNumber.py

--- randomNumber.py
+++ randomNumber.py
@@ -4,7 +4,7 @@ A simple project where the user guess the right number
 import random
 
 try:
-    right=random.sample(range(1, 10), 3)
+    right=random.sample(range(1, 100), 3)
     n=random.sample(range(0, 3), 3) 
     #Uncomment to know the answer:
     #print(n)
