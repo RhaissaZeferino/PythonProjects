883ed55d97d52c5bb582a74c3e16bc0a11d5f88d
Rhaíssa Zeferino <66739812+RhaissaZeferino@users.noreply.github.com>
2022-03-25 08:54:40 -0300

    Add files via upload

---
+++ randomNumber.py
@@ -0,0 +1,21 @@
+'''
+A simple project where the user guess the right number 
+'''
+import random
+
+try:
+    right=random.sample(range(1, 10), 3)
+    n=random.sample(range(0, 3), 3) 
+    #Uncomment to know the answer:
+    #print(n)
+    #print(right)
+    print(right[n[0]],right[n[1]],right[n[2]])
+    chosed = int(input('Please input your chosed number: '))
+    if right[0]==chosed:
+        print('You got it! Nice guess')
+    else:
+        print('Maybe next time! The answer was {}'.format(right[0]))
+except Exception as msg:
+    print('Error: {}'.format(msg))
+    
+
